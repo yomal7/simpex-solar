16b03722d7ed052b8a7cc47811af01ce4d52ef94
Oshada Yomal <140165099+yomal7@users.noreply.github.com>
2024-08-02 12:30:06 +0530

    manager ui template

---
+++ UI/customer.js
@@ -0,0 +1,22 @@
+const customers = [
+    {
+        image:  'https://st3.depositphotos.com/15648834/17930/v/450/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg',
+        name: 'Anura Kumara',
+        status: 'Feasibility Test'
+    },
+    {
+        image: 'https://st3.depositphotos.com/15648834/17930/v/450/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg' ,
+        name: 'Sajith Premadasa',
+        status: 'Document Submission'
+    },
+    {
+        image: 'https://st3.depositphotos.com/15648834/17930/v/450/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg' ,
+        name: 'Sanath Nishantha',
+        status: 'System Verification'
+    },
+    {
+        image:  'https://st3.depositphotos.com/15648834/17930/v/450/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg',
+        name: 'Ranil Wickramasighe',
+        status: 'System Verification'
+    }
+]
