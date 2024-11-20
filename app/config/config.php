4fa421f4873ea0d28bf6c117f797b3e2c44c8975
Bin4yi <habdthilakasiri2002@gmail.com>
2024-11-20 12:48:04 +0530

    change the database name

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASSWORD', '');
-    define("DB_NAME", "mvc-framework");
+    define("DB_NAME", "simpex_db");
 
     //Addresses
     define('APPROOT', dirname(dirname(__FILE__)));
