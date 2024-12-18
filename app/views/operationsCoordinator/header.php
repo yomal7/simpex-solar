2aa026e8a6c2e8ad9c40e99a99f23c79e787646d
Thisum Dinujaya <140166006+ThisumDinujaya@users.noreply.github.com>
2024-12-18 13:36:23 +0530

    add favicon image

--- app/views/operationsCoordinator/header.php
+++ app/views/operationsCoordinator/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="<?php echo URLROOT; ?>/assets/images/favicon.png">
     <title><?php echo SITENAME; ?></title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
