e2ae7cdca353814ad35082cf5490531ae9190567
yomal7 <yomaloshada45@gmail.com>
2025-02-04 14:04:45 +0530

    style: update dashboard CSS for supplier coordinator view

--- app/views/supplierCoordinator/v_dashboard.php
+++ app/views/supplierCoordinator/v_dashboard.php
@@ -1,5 +1,5 @@
 <?php require APPROOT . '/views/supplierCoordinator/header.php'; ?>
-<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/operationsCoordinator/dashboard.css">
+<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/supplierCoordinator/dashboard.css">
 </head>
 
 <body>
