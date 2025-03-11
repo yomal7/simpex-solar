edbe8991fa2a522981f38849a06aadc27473e339
BinulaDimantha <habdthilakasiri2002@gmail.com>
2025-03-11 20:00:25 +0530

    add otp message system for register

--- app/config/config.php
+++ app/config/config.php
@@ -13,3 +13,8 @@ define("DB_NAME", "simpex_db");
 define('APPROOT', dirname(dirname(__FILE__)));
 define('URLROOT', 'http://localhost/simpex-solar');
 define('SITENAME', 'simpex ');
+
+define('SMTP_HOST', 'smtp.gmail.com'); 
+define('SMTP_USER', 'pasansanjiiwa2023@gmail.com');
+define('SMTP_PASS', 'ygkj tcso qcde bzgd'); 
+define('SITE_NAME', 'simpex solar'); 
