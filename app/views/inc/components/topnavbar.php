27da7bc0d2a6f27c52631671a8793f95cbfded18
BinulaDimantha <habdthilakasiri2002@gmail.com>
2025-04-18 18:02:23 +0530

    Add user profile

--- app/views/inc/components/topnavbar.php
+++ app/views/inc/components/topnavbar.php
@@ -69,7 +69,8 @@
               <li id="auth-section">
                 <?php if ($isLoggedIn): ?>
                     <div class="profile-section" id="profile-section">
-                        <img src="<?php echo $profilePicture; ?>" alt="Profile" class="profile-pic" id="profile-pic">
+                        <!-- <img src="<php echo $profilePicture; ?>" > -->
+                        <img src="<?php echo !empty($data['customer']->profile_picture) ? URLROOT . '/public/uploads/profile_pictures/' . $data['customer']->profile_picture : URLROOT . '/public/assets/profile.png'; ?>" alt="Profile" class="profile-pic" id="profile-pic"">
 
                         <div class="profile-dropdown">
                             <ul>
