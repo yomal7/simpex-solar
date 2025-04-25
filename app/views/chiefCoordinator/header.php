172cfba19bb16bd7982f7ac12e2a52c8a559ce44
Thisum Dinujaya <140166006+ThisumDinujaya@users.noreply.github.com>
2025-04-26 02:42:17 +0530

    Refactor CSS styles for side bars of Operations Coordinator, Supplier Coordinator, and Technician pages

--- app/views/chiefCoordinator/header.php
+++ app/views/chiefCoordinator/header.php
@@ -9,5 +9,5 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/operationsCoordinator.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/chiefCoordinator/styles.css">
     <script defer src="https://cdn.jsdelivr.net/npm/chart.js"></script>
--- app/views/supplierCoordinator/header.php
+++ app/views/supplierCoordinator/header.php
@@ -8,5 +8,5 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/operationsCoordinator.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/styles.css">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/notification.css">
